2031290ebda811d32897844bfad334194c2a6904
Zabi Sideqi <148572540+Zabi-23@users.noreply.github.com>
2024-11-17 13:41:06 +0100

    uppdate .env

--- server/index.js
+++ server/index.js
@@ -10,23 +10,23 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Specifik CORS-konfiguration med alla nödvändiga inställningar
+
 const corsOptions = {
   origin: "https://portfolio-client-ochre-rho.vercel.app", // Din Vercel klient-URL
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: ["Content-Type", "Authorization"], // Se till att alla nödvändiga headers tillåts
+  allowedHeaders: ["Content-Type", "Authorization"], 
   credentials: true
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// En enkel route för att kontrollera om servern körs
+
 app.get('/', (req, res) => {
   res.send('Server is running. Use /api/v1/portfolio for API routes.');
 });
 
-// Använd portfolio-routern
+
 app.use('/api/v1/portfolio', portfolioRouter);
 
 app.listen(PORT, () => {
