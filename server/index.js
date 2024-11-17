a8e8dc1f8633c6f4e3ea7e28bb1ca4edfce444e8
Zabi Sideqi <148572540+Zabi-23@users.noreply.github.com>
2024-11-17 18:53:04 +0100

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,33 +1,22 @@
+
+
 import express from 'express';
 import cors from 'cors';
-import portfolioRouter from './routes/portfolioRoute.js';
+import portfolioRouter from './routes/portfolioRoute.js'; // Importera din router
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Lista över tillåtna ursprung
-const allowedOrigins = [
-  'http://localhost:5173', // För utvecklingsmiljö
-  'https://portfolio-clent.vercel.app', // Din live front-end domän
-  'https://portfolio-clent-git-main-zabi-23s-projects.vercel.app',
-  'https://portfolio-clent-jla10zs7d-zabi-23s-projects.vercel.app',
-  'https://portfolio-clent-i2fmnyl57-zabi-23s-projects.vercel.app'
-];
-
 // Middleware för CORS
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  }
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors({
+  origin: '*', // Tillåt alla ursprung (OBS: detta är inte säkert för produktion, men vi kan göra detta för att felsöka)
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
+// Middleware för att hantera OPTIONS-förfrågningar (preflight requests)
+app.options('*', cors());
 
 // Middleware för att hantera JSON
 app.use(express.json());
