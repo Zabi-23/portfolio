e234ae8cbce412a36e349db1e0e262a58a35f1fb
Zabi Sideqi <148572540+Zabi-23@users.noreply.github.com>
2024-11-18 13:57:46 +0100

    update index.js, vercel.json

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-
+/* 
 //server/index.js
 
 import express from 'express';
@@ -32,3 +32,31 @@ app.listen(PORT, () => {
   console.log(`Server Running On PORT ${PORT}`);
 });
 
+ */
+
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import portfolioRouter from './routes/portfolioRoute.js';
+
+dotenv.config();
+
+const app = express();
+const corsOptions = {
+  origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// A simple route to check if the server is running
+app.get('/', (req, res) => {
+  res.send('Server is running. Use /api/v1/portfolio for API routes.');
+});
+
+// Use the portfolio router
+app.use('/api/v1/portfolio', portfolioRouter);
+
+// Export Express app for Vercel
+export default app;
